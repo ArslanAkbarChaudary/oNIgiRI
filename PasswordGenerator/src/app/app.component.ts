845b2e296ef828b179aa329a2893bc2f41b65238
Arslan Akbar Chaudary <cs.programmer134@gmail.com>
2021-06-23 14:49:33 +0500

    Completed full working application

--- PasswordGenerator/src/app/app.component.ts
+++ PasswordGenerator/src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PasswordGenerator';
+  generatedPassword = "";
+  length = 0;
+  includeLatters = false;
+  includeSymbols = false;
+  includeNumbers = false;
+
+  onChangelength(event:Event){
+    const parseValue = parseInt((event.target as HTMLInputElement).value);
+    if (!isNaN(parseValue)){
+     return this.length = parseValue
+    }
+    return "Invalid length";
+  }
+
+  onSelectUseLatters(){
+    return this.includeLatters = !this.includeLatters;
+  }
+  onSelectUseNumber(){
+    return this.includeNumbers = !this.includeNumbers;
+  }
+  onSelectUseSymbols(){
+    return this.includeSymbols = !this.includeSymbols;
+  }
+
+  onGeneratePasswordClick(){
+    let randomCharsetString = '';
+    const numbers = '1234567890';
+    const latters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@£$%^&*()_+}{|":>?<';
+    if (this.includeLatters){
+       randomCharsetString += latters;
+    }if(this.includeNumbers){
+       randomCharsetString += numbers;
+    }if(this.includeSymbols){
+      randomCharsetString += symbols;
+    }
+    for(let i=0; i < this.length; i++){
+      let index = (Math.floor(Math.random() * randomCharsetString.length));
+      this.generatedPassword += randomCharsetString[index];
+    }
+  }
 }
